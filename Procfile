c1fef051f5389f8f213b640d6e2ebfa4cf6ffc97
CreepyLegend57 <manyudzajoe2018@gmail.com>
2023-06-11 17:11:21 +0200

    add new changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'creepy_auction.wsgi'
--- creepy_auction/settings.py
+++ creepy_auction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ee%y_r-c#hf5oo_f-qw^xvf-d)#e#!j9js-&1y3jql^vq=a)u0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', ' Manyudzajoe.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,7 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
