32ad14afef2eb7ecb61fb5f4bb69ad1e4379f216
CreepyLegend57 <manyudzajoe2018@gmail.com>
2023-06-11 18:26:51 +0200

    add new changes

--- creepy_auction/settings.py
+++ creepy_auction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ee%y_r-c#hf5oo_f-qw^xvf-d)#e#!j9js-&1y3jql^vq=a)u0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
